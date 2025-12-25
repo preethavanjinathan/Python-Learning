3de61bd429907e3032eded62bcf5173e1cb87f86
preethavanjinathan <preethavanjinathan@gmail.com>
2025-12-25 21:51:35 +0530

    Add files via upload

---
+++ quotientandremainder.py
@@ -0,0 +1,6 @@
+a = 10
+b = 2
+quotient = a / b
+remainder = a % b
+print("Quotient:", quotient)
+print("Remainder:", remainder)
---
+++ store2numbers_subtractandmultiply.py
@@ -0,0 +1,4 @@
+a = 100
+b = 50
+print("Difference:", a -b)
+print("Product:", a * b) 
